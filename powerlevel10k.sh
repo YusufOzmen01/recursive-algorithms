fc48ab8ecb90d71bd5ff040d97c50c899ac3eb71
Yusuf Çınar Özmen <marulumbenaq@gmail.com>
2021-07-04 17:04:30 +0300

    Update powerlevel10k.sh

--- powerlevel10k.sh
+++ powerlevel10k.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install zsh git -y
+sudo apt install zsh git curl -y
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/powerlevel10k
 echo 'source ~/.config/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 chsh --shell /usr/bin/zsh $USER
