b49a2108eccf5c961ab6ac56eaa04052eb9ab2f2
Yusuf Çınar Özmen <marulumbenaq@gmail.com>
2021-07-04 17:03:41 +0300

    Add files via upload

---
+++ powerlevel10k.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install zsh git -y
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/powerlevel10k
+echo 'source ~/.config/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+chsh --shell /usr/bin/zsh $USER
+zsh
